18b8ac4a944910d4824c4d64ecb1ee441f62c2df
Semaj Andrews <semajzandrews@gmail.com>
2022-02-10 04:52:36 -0500

    Created CoursesApp Project

---
+++ CoursesApp/CoursesApp/CoursesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoursesAppApp.swift
+//  CoursesApp
+//
+//  Created by Semaj Andrews on 2/10/22.
+//
+
+import SwiftUI
+
+@main
+struct CoursesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
