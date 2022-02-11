fefa33c924b3d03fd35ae78bf435ee8beea4dfc2
Semaj Andrews <semajzandrews@gmail.com>
2022-02-11 18:38:59 -0500

    Added Styling to CardView: shadow, blend, scale and rotation effects.

--- CoursesApp/CoursesApp/ContentView.swift
+++ CoursesApp/CoursesApp/ContentView.swift
@@ -12,25 +12,48 @@ struct ContentView: View {
         
         ZStack {
 
+            HStack {
+                
+                Spacer()
+            }
+            
             BackCardView()
+                .frame(width: 340, height: 220)
+                .background(Color("card4"))
+                .cornerRadius(20)
+                .shadow(radius: 20)
                 .offset(x: 0, y: -40)
                 .scaleEffect(0.9)
                 .rotationEffect(.degrees(10))
+                .rotation3DEffect(.degrees(10), axis: (x: 10.0, y: 0, z: 0))
+                .blendMode(.hardLight)
             
             BackCardView()
+                .frame(width: 340, height: 220)
+                .background(Color("card3"))
+                .cornerRadius(20)
+                .shadow(radius: 20)
                 .offset(x: 0, y: -20)
                 .scaleEffect(0.95)
                 .rotationEffect(.degrees(5))
+                .rotation3DEffect(.degrees(5), axis: (x: 10.0, y: 0, z: 0))
+                .blendMode(.hardLight)
             
             CardView(courseName: "UI Design")
-            
+                .frame(width: 340, height: 220)
+                .background(Color.black)
+                .cornerRadius(20)
+                .shadow(radius: 20)
+                 
         }
+
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .previewDevice("iPhone 13")
     }
 }
 
@@ -41,9 +64,10 @@ struct CardView: View {
     var body: some View {
         VStack {
             HStack {
-                VStack {
+                VStack(alignment: .leading){
                     Text("\(courseName)")
                         .font(.title)
+                        .fontWeight(.semibold)
                         .foregroundColor(.white)
                     Text("Certificate")
                         .foregroundColor(Color("accent"))
@@ -54,15 +78,15 @@ struct CardView: View {
             .padding(.horizontal, 20)
             .padding(.top, 20)
             Spacer()
+            
             Image("Card1")
                 .resizable()
                 .aspectRatio(contentMode: .fill)
-                .frame(width: 330.0, height: 110.0, alignment: .top)
+                .frame(width: 300.0, height: 110.0, alignment: .top)
         }
-        .frame(width: 340.0, height: 220.0)
-        .background(Color.black)
-        .cornerRadius(20)
-        .shadow(radius: 20)
+        
+
+        
     }
 }
 
@@ -71,9 +95,6 @@ struct BackCardView: View {
         VStack {
             Spacer()
         }
-        .frame(width: 340.0, height: 220.0)
-        .background(Color.blue)
-        .cornerRadius(20)
-        .shadow(radius: 20)
+
     }
 }
